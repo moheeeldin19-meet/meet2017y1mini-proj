315f478fa66be0cfc7770d3699645d2698988bd6
moheeeldin19-meet <moheeeldin19@meet.mit.edu>
2017-08-07 11:41:40 +0300

    Stopping here

---
+++ hi.py
@@ -0,0 +1,16 @@
+import turtle, random
+
+colors  = ["red","green","blue","orange","purple","pink","yellow","cyan","magenta", "black", "gray"] # Make a list of colors to picvk from
+
+turtle.width(10) #What does this line do?
+
+length = 5
+
+for count in range(1000):
+  color = random.choice(colors) #Choose a random color
+  turtle.forward(length)
+  turtle.right(135)
+  turtle.color(color) # Why is color spelt like this?
+  length = length + 5
+  
+  
